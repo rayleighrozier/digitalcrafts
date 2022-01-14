2a422e02f71023266f7224944dcf74f9477cb1e9
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-01-14 16:19:30 -0500

    adding the rest of day 5

---
+++ week1/day5/dictionaries.py
@@ -0,0 +1,44 @@
+# digitalcrafts = {
+#     "US":
+#         {
+#             "Orlando": "Epcot Communicore",
+#             "Houston": "123 Canal Street"
+#         },
+#     "UK":
+#         {
+#             "London": "123 Baker Street"
+#         }
+# }
+# print (digitalcrafts.keys())
+# print (digitalcrafts.values())
+# print (digitalcrafts["US"]["Orlando"])
+
+myself = {
+    "name": "Rayleigh",
+    "height": "5 foot 4 inches",
+    "eye color": "hazel",
+    "hair color": "blonde",
+    "location" : {
+        "state": "Georgia",
+        "city" : "Smyrna",
+        "streetNumber": "123",
+        "street": "Sesame Street",
+        "type": "house",
+    },
+    "siblings": {
+        "littleBrother" : {
+                "name": "Zach",
+                "height": "6 foot 1 inches",
+                "eye color": "hazel",
+                "hair color": "brown",
+                "location" : {
+                    "state": "Georgia",
+                    "city" : "Kennesaw",
+                    "streetNumber": "123",
+                    "street": "Zachs Street",
+                    "type": "house",
+                }
+        }
+    }
+}
+
