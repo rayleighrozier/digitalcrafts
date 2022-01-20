77b10b684f8bee4a65a29d7f5abeab15e16992e2
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-01-19 23:31:52 -0500

    Solved groceryApp.py
    functioning, not pretty yet

--- week2/day3/groceryApp.py
+++ week2/day3/groceryApp.py
@@ -1,36 +1,58 @@
+#CLASSES
 class GroceryList:
     def __init__(self, store, items = []):
         self.store = store
         self.items = items
-
-    def displayList(self):
-        print("hi")
+    
+class GroceryItem:
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
 
 def printAllLists():
-    print(allLists)
-    for list in allLists:
-        print(f"""
-        Grocery List For  {list.store}
-        
-        {list.items}
+    if len(allLists) >0:
+        print ("""
+    =================
         """)
-        
-def addNewList():
-    whichStore = input("Which store would you like to make a grocery list for?")
-    newStore = GroceryList(whichStore)
-    allLists.append(newStore)
+        for list in allLists:
+            print(f"""
+    Grocery List For {list.store.capitalize()} :
+         """)
+            for item in list.items:
+                print(f"""
+    {item.name} ({item.quantity})
+                 """) 
+        print ("""
+    =================
+        """)
+    else:
+        print("No lists yet! Try making one.")
 
+#working
+def addNewList():
+    whichStore = input("Which store would you like to make a grocery list for? ")
+    newStore = GroceryList(whichStore)
+    allLists.append(newStore)   
+    print(f"""
+    New grocery list created for {whichStore}.
+    =================
+    """)
+
+
+#VARIABLES
 allLists = []
 
+#PROGRAM STARTS TO RUN
 while True:
     userInput = input("""
-    GROCERY APP
+    GROCERY LIST APP
     =================
-    1. Look at my grocery lists
-    2. Add new grocery list
-    3. Edit a grocery list
-    4. Quit
-    ================
+    1. Look at my lists
+    2. Make a new list
+    3. Add item to a list
+    4. Delete item from a list
+    5. Quit
+    =================
     What do you want to do?
     """)
     try:
@@ -39,15 +61,85 @@ while True:
             printAllLists()
         elif userInput == 2:
             addNewList()
-            #will create an object using the class then add it to the allLists []
         elif userInput == 3:
-            print ("you chose 3")
+            print ("""
+    =================
+        """)
+            counter = 1
+            for list in allLists:
+                print(f""" 
+            {counter}. {list.store.capitalize()}
+                """)
+                counter = counter + 1
+            userInput3 = int(input ("Which grocery list do you want to update?"))
+            currentList = allLists[userInput3 - 1]
+            print ("""
+    =================
+        """)
+            print(f"""
+    Grocery List For {currentList.store.capitalize()} :
+         """)
+            for item in currentList.items:
+                print(f"""
+    {item.name} ({item.quantity})
+                 """) 
+            print ("""
+    =================
+        """)
+            itemName = input("What would you like to add to the list? ")
+            itemQuantity = input("How many do you need? ") 
+            newItem = GroceryItem (itemName, itemQuantity)
+            currentList.items = currentList.items + [newItem]
+            print ("""
+    =================
+        """)
+            print(f""" 
+    Updated Grocery List For {currentList.store.capitalize()} :
+            """)
+            for item in currentList.items:
+                print(f"""
+    {item.name} ({item.quantity})
+                 """) 
+            print ("""
+    =================
+        """)
         elif userInput == 4:
+            print ("""
+    =================
+        """)
+            counter = 1
+            for list in allLists:
+                print(f""" 
+            {counter}. {list.store.capitalize()}
+                """)
+                counter = counter + 1
+            userInput4 = int(input ("Which grocery list do you want to edit?"))
+            currentList = allLists[userInput4 - 1]
+            print ("""
+    =================
+        """)
+            print(f"""
+    Grocery List For {currentList.store.capitalize()} :
+         """)
+            counter = 1
+            for item in currentList.items:
+                print(f"""
+    {counter}. {item.name} ({item.quantity})
+                 """)
+                counter = counter + 1 
+            print ("""
+    =================
+        """)
+            itemName = int(input("Which item would you like to delete?"))
+            del(currentList.items[itemName - 1])
+            print(currentList.items)
+            
+
+        elif userInput == 5:
             print("Bye!")
             break
         else:
-            print("Oops! Pick a number 1-4.")
+            print("Oops! Try again.")
     except:
-         print ("Oops! Pick a number 1-4.")
+         print ("Oops! Try again.")
     
-    # store = input("What store would you like to make a shopping list for?")
