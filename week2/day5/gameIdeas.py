8944542dae35352bb97bfda53d52389ebfd0e254
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-01-21 17:55:13 -0500

    updated gameIdeas.py with scoring mechanism

--- week2/day5/gameIdeas.py
+++ week2/day5/gameIdeas.py
@@ -7,7 +7,8 @@
 #put it after every win
 
 # DAWGS ON TOP
-#Glory Glory to ol' Georgia! After a historic championship season, Kirby Smart has retired as head coach of the Georgia Bulldogs.
+#*music note* Glory Glory to ol' Georgia! *music note
+# After a historic championship season, Kirby Smart has retired as head coach of the Georgia Bulldogs.
 #The coaching search was long and tedious, but UGA has selected you as the next head coach.
 #Will you lead the Dawgs to another trophy? Or stumble against your most hated rivals (*cough* Alabama) ? The road to the natty starts now! 
 
@@ -21,7 +22,7 @@
     # Balanced approach is best (neutral offense = 2, defense = 2 )
 
 #experience starts at zero, you get more as you win
-#how will experience mechanic work? will experience mechanic lead to better rolls
+
 
 #for every game
 #Who are you starting at quarterback 
@@ -42,25 +43,88 @@
     
 #Opponent Score == random number out of their teams range minus whatever your defense did
 #If your score is bigger than theirs, you win the game (and gain one experience point)
+import random
+class Team:
+    def __init__(self, coachName, offense = 2 , defense = 2, experience = 0, wins =0):
+        self.coachName = coachName
+        self.offense = offense
+        self.defense = defense
+        self.experience = experience
+        self.wins = wins
+    
+    def getScore (self,rival):
+        offenseMin = 3 + (self.experience * 7) # minimum at start is 3
+        offenseMax = 14 + (self.offense * 7) # maximum at start is between 14 and 35
+        score = random.randint(offenseMin, offenseMax)
+        def defenseRoll (rival):
+            probability = [True,False,False,False,False,False,False] # min at start is 1 in 7
+            counter = rival.experience
+            while counter > 0 and len(probability) > 2:
+                probability.pop()
+                counter = counter-1
+            roll = random.choice(probability)
+            return roll
+        counter = rival.defense #min at start is 1 , max is 3
+        while counter > 0:
+            roll = defenseRoll(rival)
+            if roll == True and score >= 7:
+                score = score - 7
+            elif roll == True and score >= 3:
+                score = score - 3
+            else:
+                score = score
+            counter = counter - 1
+        return score
+    
+Georgia = Team("Georgia") 
+Oregon = Team("Oregon")
 
+#stuff to test hard mode team
+# Oregon.experience = 5
+# Oregon.defense = 8
+# Oregon.offense = 10
+
+print (f"""
+FINAL SCORE:
+Georgia - {Georgia.getScore(Oregon)}
+Oregon - {Oregon.getScore(Georgia)}
+""")
+
+# def defenseRoll2():
+#     defProbability = [True,False,False,False,False,False,False]
+#     counter = opposingTeam.experience
+#     while counter > 0 and len(defProbability) > 2:
+#         defProbability.pop()
+#         counter = counter-1
+#     roll = random.choice(defProbability)
+#     return roll
+
+# #for every point in defense, you get a defense roll 
+   
+# offMin = 3 + (fakeUGA.experience * 7) #might need to lower to 3
+# offMax = 30 + (fakeUGA.offense * 7) #might need to lower to 3
+# print(f"minimum is {offMin} and maximum is {offMax}")
+# opposingTeam.defense =  8
+# opposingTeam.experience = 3
+# def getScore ():
+#     score = random.randint(offMin, offMax)
+#     counter = opposingTeam.defense
+#     while counter > 0:
+#         roll = defenseRoll()
+#         if roll == True and score >= 7:
+#             score = score - 7
+#         elif roll == True and score >= 3:
+#             score = score - 3
+#         counter = counter - 1
+#     return score
+# getScore()
 
 
 
 #game one oregon
 # ++ defense but no experience 
 
+# 
 
-
-class Team:
-    def __init__(self, coachName, offense = 2 , defense = 2, experience = 0):
-        self.coachName = coachName
-        self.offense = offense
-        self.defense = defense
-        self.experience = experience
-
-
-class rivalTeam (Coach):
-    print("hi")
-    #no name needed I guess
     
 
