154630a31e108c87d50cbb683af9cfa7b1e6f495
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-02-21 12:32:40 -0500

    function to randomly select songs and display them

--- week7/musicMoodRing/app.js
+++ week7/musicMoodRing/app.js
@@ -1,3 +1,5 @@
+const container = document.querySelector(".container");
+
 const getToken = async () => {
   const result = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
@@ -8,52 +10,47 @@ const getToken = async () => {
     body: "grant_type=client_credentials",
   });
   const data = await result.json();
-  console.log(data);
   return data.access_token;
 };
 
-const getGenres = async () => {
+const getSong = async (playlistID) => {
   const token = await getToken();
   const result = await fetch(
-    `https://api.spotify.com/v1/browse/categories?locale=sv_US`,
-    {
-      method: "GET",
-      headers: { Authorization: "Bearer " + token },
-    }
-  );
-
-  const data = await result.json();
-  console.log(data.categories.items);
-  return data.categories.items;
-};
-getGenres();
-
-const browseCategories = async () => {
-  const token = await getToken();
-  const result = await fetch(`https://api.spotify.com/v1/browse/categories`, {
-    method: "GET",
-    headers: { Authorization: "Bearer " + token },
-  });
-  const data = await result.json();
-  console.log(data);
-  return data;
-};
-browseCategories();
-
-const getHappyHits = async () => {
-  const token = await getToken();
-  const result = await fetch(
-    `https://api.spotify.com/v1/playlists/37i9dQZF1DXdPec7aLTmlC/tracks`,
+    `https://api.spotify.com/v1/playlists/${playlistID}/tracks`,
     {
       method: "GET",
       headers: { Authorization: "Bearer " + token },
     }
   );
   const data = await result.json();
-  let songs = data.items;
-  for (song of songs) {
-    console.log(song.track.name, song.track.artists[0].name);
+  let playlistSongs = data.items;
+  let song =
+    playlistSongs[Math.floor(Math.random() * playlistSongs.length)].track;
+  console.log(song);
+  let name = song.name;
+  let artists = song.artists[0].name;
+  if (song.artists.length > 1) {
+    for (i = 1; i < song.artists.length; i++) {
+      artists = artists + ", " + `${song.artists[i].name}`;
+    }
   }
+  console.log(artists);
+  let cover = song.album.images[0].url;
+  let songCard = document.createElement("div");
+  songCard.className = "song-card";
+  let cardName = document.createElement("p");
+  cardName.innerText = name;
+  let cardArtists = document.createElement("p");
+  cardArtists.innerText = artists;
+  let cardCover = document.createElement("img");
+  cardCover.className = "card-cover";
+  cardCover.src = cover;
+  let cardButton = document.createElement("button");
+  cardButton.innerText = "Listen on Spotify";
+  songCard.append(cardCover, cardName, cardArtists, cardButton);
+  container.append(songCard);
   return data;
 };
-getHappyHits();
+getSong("37i9dQZF1DXdPec7aLTmlC");
+
+// getPlaylist("37i9dQZF1DX6VdMW310YC7");
