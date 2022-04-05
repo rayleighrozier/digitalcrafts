9b504db97556622b44ee8beb96c60db437f9749c
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-05 17:38:46 -0400

    profile styling

--- week13/instagram-clone/src/components/Profile.js
+++ week13/instagram-clone/src/components/Profile.js
@@ -5,12 +5,16 @@ export default function Profile({ profile }) {
     <div className="profile">
       <img src={profile.img} />
       <div className="profile-details">
-        <p>{profile.handle}</p>
-        <button>Edit Profile</button>
-        <button>Gear</button>
-        <p>{profile.posts} posts </p>
-        <p>{profile.following} following </p>
-        <p>{profile.followers} followers </p>
+        <div className="profile-top">
+          <p>{profile.handle}</p>
+          <button>Edit Profile</button>
+          <button>Gear</button>
+        </div>
+        <div className="profile-count">
+          <p>{profile.posts} posts </p>
+          <p>{profile.following} following </p>
+          <p>{profile.followers} followers </p>
+        </div>
         <p>
           {profile.firstName} {profile.lastName}
         </p>
